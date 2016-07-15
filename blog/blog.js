32e1ef3e0cfe4fe7d69287ec59162261ebad3496
Nour Mardini <nour.mardini@gmail.com>
2016-07-14 18:46:48 -0700

    Week5 work

--- blog/blog.js
+++ blog/blog.js
@@ -2,9 +2,9 @@
 var link = document.querySelector(".like-link");
 //Declare a variable representing the like count
 var likeCount = document.querySelector(".like-count");
-//Declare a variable representing the new comment
+//Declare a variable representing the new comment form
 var form = document.querySelector("#new-comment");
-//Declare a variable for the 
+//Declare a variable for the text in the new form's body
 var commentBody = document.querySelector("#new-comment-body");
 
 link.addEventListener("click", like);
--- todo-list/script.js
+++ todo-list/script.js
@@ -1,10 +1,24 @@
 var form = document.querySelector("#new-item-form");
+var formInput = document.querySelector("#new-item-input");
+var button = document.querySelector("button");
+var body = document.querySelector("body");
+var unorderList = document.querySelector("#todo-list");
 
 function formSubmitted(event) {
   event.preventDefault();
+  var listItem = document.createElement("li");
+  var checkbox = document.createElement("input");
+  var span = document.createElement("span");
+  var label = document.createElement("label");
 
-  // Your code goes here...
+  //listItem.setAttribute("id","todo-list li");
+  checkbox.setAttribute("type","checkbox")
+  span.textContent = formInput.value;
 
+  unorderList.appendChild(checkbox);
+  unorderList.appendChild(label);
+  unorderList.appendChild(span);
+  unorderList.appendChild(listItem);
   form.reset();
 }
 
