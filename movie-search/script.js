c94503163b19767628d4da8a8f20607dee0e1a2a
Nour Mardini <nour.mardini@gmail.com>
2016-07-31 19:35:22 -0700

    Movie search

--- movie-search/script.js
+++ movie-search/script.js
@@ -1,20 +1,39 @@
 //Create variable for form and add event listener
 var form = document.querySelector("form");
+var ul = document.querySelector("ul");
 form.addEventListener("submit", lookUpMovie);
-//works till here
 
 //Create variables for inputted value and append it to base URL
 function lookUpMovie(event){
   event.preventDefault()
   var input = document.querySelector("#new-item-input");
   var url = ("http://omdbapi.com/?s=") + input.value;
-  console.log("works till here");
-  ;
-//I have no idea why this is not working
 
-//Calls get function with arguments of url variable  that we jusr defined and showInfo function
-  get(url, showMovie);
+  ul.textContent="";
+  get(url, showMovieInfo);
 }
+
+function showMovieInfo(results){
+
+//Assign variable for all parsed JSON data to turn it into an object
+  var movieResults = JSON.parse(results);
+  var moviesArray = movieResults.Search;
+  moviesArray.forEach(function(movie) {
+    var li1 = document.createElement("li");
+    var li2 = document.createElement("li");
+    li1.id = "li1";
+    var div = document.createElement("div");
+    var href = document.createElement("a");
+    li1.textContent = movie.Title;
+    li2.textContent = movie.Year;
+    href.setAttribute = ("http://omdbapi.com/?s=")+movie.Title
+
+    ul.appendChild(div);
+    div.appendChild(li1)
+    div.appendChild(li2)
+  })
+}
+
 
 //Get function executing HTTP request
 function get(url, callback) {
