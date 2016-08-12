329b136ca5c6ba401f5d7b70baad463df81785ba
Nour Mardini <nour.mardini@gmail.com>
2016-08-11 18:35:28 -0700

    in progress

--- final-project/script.js
+++ final-project/script.js
@@ -1,16 +1,73 @@
-// //FOX
-// $('polygon').each(function() {
-// 		function TimelineLite(animation){
-// 			var minY = -1000;
-// 			var maxY = 0;
 //
-// 			var minX = -1000;
-// 			var maxX = 1000;
-//
-// 			var randomY = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
-// 			var randomX = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
-// 	   animation.set(this, { transformOrigin:"50% 50%", position: "absolute", x: randomX, y: randomY, rotation: randomX, scale: 1});
-// 	   animation.to(this, 3, { x: 0, y: 0, rotation: 0, scale: 1, ease: Power4.easeInOut, y: 0 }, 0.5);
-// 	 		}
-// 	});
-// //FOX
+$("#mybutton").on("click",function(){
+  console.log("is this thing on?");
+  $("#content-creation").html("<h1>This is new</h1>");
+})
+$("#content-creation").html("<h1>This is new</h1>");
+
+
+//RAIN
+var canvas = document.getElementById("canvas");//Canvas
+var ctx = canvas.getContext("2d");//It provides the 2D rendering context for the drawing surface
+color = "#01235d"; //randomColor();
+
+var height = window.innerHeight;
+//height of the browser window's viewport
+var width = window.innerWidth;
+//width of the browser window's viewport
+
+//Setting variables for width and height of canvas
+canvas.width = width;
+canvas.height = height;
+
+//Setting variable names for rain count and drops (which is an array)
+var rain_count = 100;
+var rain_drops = [];
+
+//Whats calling this?
+//This function controls the number of raindrops, increasing them till they match the rain count set
+var generate = function() {
+    for (var i = 0; i < rain_count; i++) {
+        rain_drops.push(new rain());
+    }
+}
+
+//This function conrols the behavior of the rain drops - speed, location, acceleration
+function rain() {
+    this.speed = {
+        y: 2.5 + Math.random() * 5
+    }
+
+    this.location = {
+        x: 0 + Math.random() * width,
+        y: -100 + Math.random() * 100
+    }
+
+    this.accel = {
+        y: 1.5 + Math.random() * 3
+    }
+}
+
+var draw = function() {
+    ctx.globalCompositeOperation = "source-over";
+    ctx.fillStyle = "#7dd3e2";//Background color
+    ctx.fillRect(0, 0, width, height);
+    ctx.globalCompositeOperation = "lighter";
+
+    for (var i = 0; i < rain_drops.length; i++) {
+        var r = rain_drops[i];
+
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        ctx.rect(r.location.x, r.location.y, 0.5, 75);
+        ctx.fill();
+
+        r.speed.y += r.accel.y;
+
+        r.location.y += r.speed.y;
+    }
+}
+
+setInterval(generate, 100);
+setInterval(draw, 30);
+//End of RAIN
